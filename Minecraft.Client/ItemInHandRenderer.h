6d8e70a0491a687bcefba3d601563ef9d3300b6d
str1k3r <115313679+S1l3ntStr1ke87@users.noreply.github.com>
2026-03-05 04:36:04 -0500

    fix slight mess up (item frame render was put in this)

--- Minecraft.Client/ItemInHandRenderer.h
+++ Minecraft.Client/ItemInHandRenderer.h
@@ -1,17 +1,46 @@
 #pragma once
-#include "EntityRenderer.h"
 
-class ItemFrameRenderer : public EntityRenderer
+class Minecraft;
+class ItemInstance;
+class Minimap;
+class LivingEntity;
+class TileRenderer;
+class Tesselator;
+
+class ItemInHandRenderer
 {
+public:
+	// 4J - made these public
+	static ResourceLocation ENCHANT_GLINT_LOCATION;
+    static ResourceLocation MAP_BACKGROUND_LOCATION;
+    static ResourceLocation UNDERWATER_LOCATION;
+
 private:
-	static ResourceLocation MAP_BACKGROUND_LOCATION;
-	Icon *backTexture;
+	Minecraft *minecraft;
+    shared_ptr<ItemInstance> selectedItem;
+    float height;
+    float oHeight;
+    TileRenderer *tileRenderer;
+	static int listItem, listGlint, listTerrain;
 
 public:
-	void registerTerrainTextures(IconRegister *iconRegister);
-	virtual void render(shared_ptr<Entity> _itemframe, double x, double y, double z, float rot, float a); 
+	// 4J Stu - Made public so we can use it from ItemFramRenderer
+    Minimap *minimap;
 
+public:
+	ItemInHandRenderer(Minecraft *mc, bool optimisedMinimap = true); // 4J Added optimisedMinimap param
+    void renderItem(shared_ptr<LivingEntity> mob, shared_ptr<ItemInstance> item, int layer, bool setColor = true);	// 4J added setColor parameter
+	static void renderItem3D(Tesselator *t, float u0, float v0, float u1, float v1, int width, int height, float depth, bool isGlint, bool isTerrain);	// 4J added isGlint and isTerrain parameter
+public:
+    void render(float a);
+    void renderScreenEffect(float a);
 private:
- 	void drawFrame(shared_ptr<ItemFrame> itemFrame);
- 	void drawItem(shared_ptr<ItemFrame> entity); 
+	void renderTex(float a, Icon *slot);
+    void renderWater(float a);
+    void renderFire(float a);
+    int lastSlot;
+public:
+	void tick();
+    void itemPlaced();
+    void itemUsed();
 };
